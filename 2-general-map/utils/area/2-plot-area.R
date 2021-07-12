ed9c314e66a0a44b9e906b9f65169d59be6d26e1
Dhemerson Conciani <dhemerson.costa@ipam.org.br>
2021-07-12 11:20:47 -0300

    Update 2-plot-area.R

--- 2-general-map/utils/area/2-plot-area.R
+++ 2-general-map/utils/area/2-plot-area.R
@@ -1,133 +1,153 @@
-## perform accuracy analisys 
-## developed by IPAM
-## write to dhemerson.costa@ipam.org.br
+## compute accuracy from test datasets
+## dhemerson.costa@IPAM.org.br
 
 ## read libraries
-library (dplyr)
 library (ggplot2)
-library (sf)
+library (reshape2)
+library (dplyr)
+library (tidyr)
+library (tools)
 
-## define root directory for accuracy tables
-path <- '../tables/specific_accuracy/'
+## avoid scientific notation
+options(scipen= 999)
 
-## define spatial data
-spatial <- read_sf(dsn= '../vectors', layer= 'cerrado_c6_regions')
-plot(spatial) ## check spatial data
+## path of files
+path = "../tables/area/finalv10/"
 
-## define functions to be used along the analisys
-
-## function to read accuracy tables
-read_accTables <- function (x) {
-  ## list files into root directory
-  files <- list.files(x)
-  ## create empty recipe
+## function to bind collection data
+readCollection <- function (dir, version) {
   recipe <- as.data.frame(NULL)
-  ## read each file and merge into recipe
-  for (i in 1:length(files)) {
-    ## import tables, excluding "system:index" [1] and ".geo" [13] columns
-    table <- read.csv(paste0(path, files[i]))[-1][-13]
-    ## merge current table to recipe
-    recipe <- rbind(recipe, table)
+  
+  if (version == 'col5') {
+    
+    col_files <- list.files(dir, pattern= version, full.names= TRUE)
+    for (i in 1:length(col_files)) {
+      temp <- read.csv(col_files[i])
+      recipe <- rbind (recipe, temp)
+    }
+    recipe$col <- version
+    colnames(recipe)[11] <- 'level'
+    recipe <- recipe[-1][-9]
+    
+    recipe$wetland <- 0
+    recipe$restinga <- 0
+    return (recipe)
+  } else {
+    col_files <- list.files(dir, pattern= 'col6', full.names= TRUE)
+    for (i in 1:length(col_files)) {
+      temp <- read.csv(col_files[i])
+      recipe <- rbind (recipe, temp)
+    }
+    return (recipe[-1][-13][-13][-1])
   }
-  ## return merged tables
-  return (recipe)
 }
 
-## compute omission error mean 
-omissionMean <- function(input) {
-  temp <- input
-  ## compute mean value for omission error
-  forest_omission <- aggregate (list(temp$FOREST_OMISSION), by= list(temp$mapb), FUN= "mean")
-  savanna_omission <- aggregate (list(temp$SAVANNA_OMISSION), by= list(temp$mapb), FUN= "mean")
-  grassland_omission <- aggregate (list(temp$GRASSLAND_OMISSION), by= list(temp$mapb), FUN= "mean")
-  
-  ## rename cols
-  colnames(forest_omission)[2] <- "Mean"; colnames(forest_omission)[2] <- "Mean"; forest_omission$level <- "Omission Error" ; forest_omission$class <- "Forest"
-  colnames(savanna_omission)[2] <- "Mean"; savanna_omission$level <- "Omission Error" ; savanna_omission$class <- "Savanna"
-  colnames(grassland_omission)[2] <- "Mean"; grassland_omission$level <- "Omission Error" ; grassland_omission$class <- "Grassland"
-  
-  ## bind data
-  omission_error <- rbind (forest_omission, savanna_omission, grassland_omission)
-  colnames(omission_error)[1] <- 'mapb'
-  
-  ## join with spatial
-  omission_error <- left_join(spatial, omission_error, by= 'mapb')
-  omission_error$Mean[omission_error$Mean == 1] <- NA
-  
-  return (omission_error)
+## load collection 6 areas 
+col6 <- readCollection(dir= path, version= 'col6')
+
+## load collection 5 areas
+col5 <- readCollection(dir= path, version= 'col5')
+
+## bind data
+data <- rbind (col6, col5)
+data <- col6
+
+## melt data
+melted <- melt (data, id= c('year', 'level', 'region'))
+
+## aggregate
+statistics <- aggregate(melted$value, FUN= "sum", by= list(year= melted$year, 
+                                                           col= melted$level,
+                                                           class= melted$variable))
+
+statistics2 <- as.data.frame(NULL)
+
+## for each version
+for (i in 1:length(unique(statistics$col))) {
+  temp <- subset (statistics, col == unique(statistics$col)[i])
+  ## subset natural classes
+  temp <- subset(temp, class != "pasture" & class != "other_nonVeg")
+  ## summarize by year
+  temp <- aggregate(x= list(area= temp$x), FUN= "sum", by= list(year= temp$year))
+  temp$anthropic <- 2045000 - temp$natural 
+  ## insert version id
+  temp$level <- unique(statistics$col)[i]
+  ## bind data 
+  statistics2 <- rbind(statistics2, temp)
 }
 
-## compute commission error mean 
-commissionMean <- function(input) {
-  temp <- input
+statistics2 <- melt(statistics2, id=c("year", "level"))
 
-  ## compute mean value for commission error
-  forest_commission <- aggregate (list(temp$FOREST_COMMISSION), by= list(temp$mapb), FUN= "mean")
-  savanna_commission <- aggregate (list(temp$SAVANNA_COMMISSION), by= list(temp$mapb), FUN= "mean")
-  grassland_commission <- aggregate (list(temp$GRASSLAND_COMMISSION), by= list(temp$mapb), FUN= "mean")
+## plot natural vs anthropic
+ggplot (statistics2, aes(x=year, y= value, colour= level)) +
+  geom_line() +
+  geom_point(aes(pch=level), alpha= 0.5) +
+  scale_colour_manual(values= c('gray90', 'gray90', 'gray90', 'blue', 'gray90', 'black')) +
+  facet_wrap(~variable, scales= "free_y") +
+  theme_bw() +
+  xlab ('Year') + ylab ("Area (km²")
+
+
+### fazer subset de agua
+#statistics <- subset (statistics, class == 'water')
+#statistics <- subset (statistics, col == 'col5' | col == 'CERRADO_col6_wetlandsv6_generalv8_rect')
+### ajustar area
+#statistics$x <- statistics$x *100 - 300900
+
+## plot general
+##ggplot (subset(statistics, class== 'forest'), aes(x=year, y= x, colour= col)) +
+ggplot (data= subset(statistics, class != "restinga"), aes(x=year, y= x, colour= col)) +
+  geom_line(size=1.5) +
+  geom_point(aes(pch=col), alpha= 0.5) +
+  scale_colour_manual(values= c('green4', 'black', 'blue', 'black', 'gray90', 'black')) +
+  facet_wrap(~class, scales= "free_y") +
+  theme_bw() +
+  xlab ('Year') + ylab ("Area (km2)")
+
+
+a <- subset(melted, variable == 'wetland')
+
+## plot by region
+ggplot(data= subset(a, level != "col5"), aes(x= year, y= value, colour= level)) +
+  geom_line() +
+  geom_point(aes(pch=level), alpha= 0.5) +
+  facet_wrap(~region, scales= "free_y") +
+  scale_colour_manual(values= c('green4', 'black', 'black', 'blue', 'gray90', 'black')) +
+  theme_bw() +
+  xlab ('Year') + ylab ("Area (km²)")
   
-  ## rename cols
-  colnames(forest_commission)[2] <- "Mean"; forest_commission$level <- "Commission Error" ; forest_commission$class <- "Forest"
-  colnames(savanna_commission)[2] <- "Mean"; savanna_commission$level <- "Commission Error" ; savanna_commission$class <- "Savanna"
-  colnames(grassland_commission)[2] <- "Mean"; grassland_commission$level <- "Commission Error" ; grassland_commission$class <- "Grassland"
-  
-  ## bind data
-  commission_error <- rbind (forest_commission, savanna_commission, grassland_commission)
-  colnames(commission_error)[1] <- 'mapb'
-  
-  ## join with spatial
-  commission_error <- left_join(spatial, commission_error, by= 'mapb')
-  commission_error$Mean[commission_error$Mean == 1] <- NA
-  
-  return (commission_error)
-}
 
-## import accuracy tables
-## parameter x refers to the root directory where CSV files are stored
-data <- read_accTables(x= path) 
 
-## assign spatial reference to accuracy table
-## for this, you need to specify the field tht corresponds to the "region ID" in the CSV and spatial data
-## in the mapibomas cerrado case we use the field "mapb"
-data <- left_join (spatial, data, by= "mapb")
 
-## compute omission  and commission errors mean by class
-omission_error <- omissionMean(input= data)
-commission_error <- commissionMean(input= data)
+## plot wetlands by region
+ggplot(subset(data, level == "pseudointegration" | level == "col5"),
+       aes(x=year, y= wetland, colour= level, group= level)) +
+  geom_line() +
+  geom_point(alpha= 0.5) +
+  scale_colour_manual(values= c('black', 'red', 'red','gray80', 'gray80', 'black', 'gray80', 'gray80', 'gray80')) +
+  facet_wrap(~region, scales= "free_y") +
+  #ggtitle('Wetland') +
+  theme_bw()
 
-## plot map of global accuracy by year
-x11()
-ggplot () +
-  geom_sf(data=na.omit(data), aes(fill=GLOBAL_ACC), color="gray50", size= 0.1, alpha=0.9) + 
-  facet_wrap(~YEAR, ncol=7) + 
-  scale_fill_fermenter(n.breaks=6, palette= "RdYlGn", na.value="gray90", direction = 1) +
-  labs(fill="Global accuracy")  + 
-  ggtitle("Cerrado V7 (without wetlands)") +
-  theme_minimal()
+## subset only to tests
+data2 <- subset (data, level == "pseudointegration")
+melted2 <- melt (data2, id= c('year', 'level', 'region'))
+statistics2 <- aggregate(melted2$value, FUN= "sum", by= list(year= melted2$year, 
+                                                            col= melted2$level,
+                                                            class= melted2$variable))
 
-## plot boxplot of global accuracy by year
-ggplot (data, aes(x= as.factor(YEAR), y= GLOBAL_ACC)) +
-  geom_jitter(alpha=0.1) +
-  geom_boxplot(fill='green4', alpha=0.5) +
-  theme_minimal() +
-  xlab('Year') + ylab ('Global Accuracy') +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-## plot mean omission by class
-ggplot () +
-  geom_sf(data=na.omit(omission_error), aes(fill=Mean), color="gray50", size= 0.1, alpha=0.9) + 
-  facet_wrap(~class, ncol=7) + 
-  scale_fill_fermenter(n.breaks=6, palette= "RdYlGn", na.value="gray90", direction = -1) +
-  labs(fill="Omission Error")  + 
-  ggtitle("Cerrado V7 (without wetlands)") +
-  theme_minimal()
+ggplot (subset(statistics2, class == "forest"), aes(x=year, y= x, colour= col)) +
+  geom_line(size=1.5) +
+  geom_point(aes(pch=col), size= 4, alpha= 0.9) +
+  scale_colour_manual(values= c('red', 'blue')) +
+  facet_wrap(~class, scales= "free_y") +
+  #ylim(62500,72500) +
+  xlab('Ano') + ylab ('Área (km²)') +
+  theme_bw()
 
-## plot mean commission by class
-ggplot () +
-  geom_sf(data=na.omit(commission_error), aes(fill=Mean), color="gray50", size= 0.1, alpha=0.9) + 
-  facet_wrap(~class, ncol=7) + 
-  scale_fill_fermenter(n.breaks=6, palette= "RdYlGn", na.value="gray90", direction = -1) +
-  labs(fill="Commission Error")  + 
-  ggtitle("Cerrado V7 (without wetlands)") +
-  theme_minimal()
+subset(statistics2, year == 2019 & class == "wetland")
+
+
+subset(statistics2, level == "CERRADO_col6_wetlandsv2_generalv8_pseudofix")
 
